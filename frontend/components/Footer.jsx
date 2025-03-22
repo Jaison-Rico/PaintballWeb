632d87a7d72ed81a4b74a226e60b7e31862490b3
Jaison-Rico <jaih90@aol.com>
2025-03-21 22:22:31 -0500

    creacion del proyecto

---
+++ frontend/components/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Footer() {
+  return (
+    <div>
+      <h1>Este es mi pie de página</h1>
+    </div>
+  )
+}
+
---
+++ frontend/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div>
+      <h1>Este es mi encabezado</h1>
+    </div>
+  )
+}
