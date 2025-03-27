dafdfd8d5dd4cbf45e3ac29963791734a6dcbc14
Jaison-Rico <jaih90@aol.com>
2025-03-26 20:15:41 -0500

    add: navbar

--- frontend/components/Header.jsx
+++ frontend/components/Header.jsx
@@ -2,8 +2,37 @@ import React from 'react'
 
 export default function Header() {
   return (
+    <header className='' >
     <div>
-      <h1>Este es mi encabezado</h1>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary "> 
+  <div className="container-fluid">
+    <a className="navbar-brand" href="#">Paintball City</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav  mb-2 mb-lg-0 ms-auto">
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">About</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Reservas</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Registrarse</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link active" aria-current="page" href="#">Contact</a>
+        </li>
+        
+      </ul>
     </div>
+  </div>
+</nav>
+    </div>
+    </header>
   )
 }
