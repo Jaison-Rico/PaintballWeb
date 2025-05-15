ddd072da7ddf3affc39b9471fe4eac67350a4627
Jaison-Rico <jaih90@aol.com>
2025-05-15 08:10:59 -0500

    Modified: se eliminaron lineas innecesarias del index

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,8 @@ import dotenv from 'dotenv'
 import { getConnection } from './config/Connection.js'
 import routerEquipos from './routes/EquiposRoute.js'
 import routerReservas from './routes/ReservasRoute.js'
-// const router = express.Router()
+
+
 const app = express()
 dotenv.config()
 
@@ -16,18 +17,3 @@ app.listen(process.env.PORT, ()=>{
     console.log(`Conectado al puerto: ${process.env.PORT}`)
     getConnection
 })
-// router.delete('/reservas/:id_reserva', (req, res ) => {
-//     const { id_reserva } = req.params
-//     try{
-//     const con = getConnection
-//     con.request()
-//     .input('id_reserva', sql.Int, id_reserva)
-//     execute('sp_eliminar_reserva')
-//     res.status(201).json({message: 'Equipo eliminado.'})
-//     }
-//     catch (error) {
-//         console.error('Error al eliminar reserva:', error);
-//         res.status(500).json({message: error.message})
-//     }
-// })
-// export default router
